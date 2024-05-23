921ae3650a6e955a06fc2dfb6a04037e8d438c49
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-23 19:59:52 +0530

    create input forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
-    <div>
-      
+    <div className="App">
+    <h1>Creating a Hero</h1>  
+    <label>Name:</label>
+    <input type="text" />
+    <label>Age:</label>
+    <input type="number" />
+    <label>Height:</label>
+    <input type="number" />
+    <label>Super Power:</label>
+    <input type="text" />
     </div>
   )
 }
